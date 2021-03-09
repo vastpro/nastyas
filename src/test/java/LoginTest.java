f818077cbf553f4c53dab7a421e8d33e58e9140c
Вадим Астахов <vastakhov@sberhealth.ru>
2021-03-09 18:25:46 +0300

    add first test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class LoginTest {
+
+    @Test
+    public void login() {
+        open("");
+    }
 }
