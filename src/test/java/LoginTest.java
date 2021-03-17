215795c80de3dd6dc1feca5c1396e968c62f4238
Anastasiya Weis <aweis@sberhealth.ru>
2021-03-17 13:12:25 +0300

    added print title

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
 
 public class LoginTest {
 
     @Test
     public void login() {
         open("https://yandex.ru/");
+        System.out.println(title());
     }
 }
