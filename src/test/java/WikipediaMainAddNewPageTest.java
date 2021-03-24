8ac573bb9565c6196ab9e8642c62ed69639116dc
Tonkikh Anastasia <atonkih@sberhealth.ru>
2021-03-24 15:57:31 +0300

    Added tests for adding new page and checking article Джон Джеймс Одюбоон

---
+++ src/test/java/WikipediaMainAddNewPageTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class WikipediaMainAddNewPageTest {
+
+    @Test
+    public void openMainPage() {
+        open("https://ru.wikipedia.org/");
+        $x("//*[contains(text(), 'Создать статью')]").shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void addNewPage(){
+        open("https://ru.wikipedia.org/");
+        $x("//*[contains(text(), 'Создать статью')]").shouldBe(Condition.visible);
+        $x("//*[contains(text(), 'Создать статью')]").click();
+        $(".firstHeading").shouldHave(Condition.text("Википедия:Мастер статей"));
+    }
+
+    @Test
+    public void openArticle(){
+        open("https://ru.wikipedia.org/wiki/Одюбон,_Джон_Джеймс");
+        $(".firstHeading").shouldHave(Condition.text("Одюбон, Джон Джеймс"));
+    }
+}
